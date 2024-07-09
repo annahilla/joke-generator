70b643a6dad5bbd5e5841749e8916e3bf99208c9
Anna Hilla <annahilla21@gmail.com>
2024-07-09 12:24:22 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import generaateJoke from './generateJoke';
+import './styles/main.scss';
+import laughing from './assets/laughing.svg';
+
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing;
+
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generaateJoke)
+
+generaateJoke()
